f2e02e2d82984797e90e04f4658e374bbcee8e48
abdulrahman-mohd2003 <alshakh.16.61@outlook.com>
2025-09-03 18:08:18 +0300

    statistics

--- Cloudy.py
+++ Cloudy.py
@@ -37,3 +37,37 @@ def filter_by_date():
         print("File 'Cloudy.csv' not found! ;( Please make sure the file exists.")
 
 filter_by_date()
+
+
+
+
+# Function for Temperature Statistics
+def display_weather_stats(new_observation):
+    temperatures = [i['temperature'] for i in new_observation]
+    weather_conditions = [i['weather_condition'] for i in new_observation]
+
+
+    # Get Maximum, Minimum, Average, and Mode (Most Frequent) in Variables
+    mode_weather = pd.Series(weather_conditions).mode()[0]
+    highest_degree = max(temperatures)
+    lowest_degree = min(temperatures)
+    average_degree = round(sum(temperatures) / len(temperatures), 2)
+
+    # Display to the user
+    while True:
+        choice = input("Choose what to display: Minimum, Maximum, Average, Most Common Weather or Exit: ")
+        try:
+            if choice.lower() == 'minimum':
+                print(f"The lowest degree is {lowest_degree}")
+            elif choice.lower() == 'maximum':
+                print(f"The highest degree is {highest_degree}")
+            elif choice.lower() == 'average':
+                print(f"The Average degree is {average_degree}")
+            elif choice.lower() == 'most common weather':
+                print(f"The most common weather is {mode_weather}")
+            elif choice.lower() == 'exit':
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print("Please choose Minimum, Maximum, Average, Most Common Weather, or Exit.")
